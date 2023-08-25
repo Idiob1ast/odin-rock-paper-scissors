fbbad4fb7711625f5e89be61e73df3c30e62cc45
Idioblast <47544178+Idiob1ast@users.noreply.github.com>
2023-08-24 20:53:43 -0700

    Create function to capitalize words Create function to play a round of the game Prompt for player's selection instead of using constant Create wrapper 'game' function to encompass everything Play 5 rounds of the game, using a loop

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-// Function to randomly return either 'Rock', 'Paper', or 'Scissors'
-function getComputerChoice() {
-    // Random number between 1 and 3 to map to the three choices, i.e. 1 = Rock, 2 = Paper, etc.
-    let computerChoice = Math.floor(Math.random() * 3) + 1
+function game() {
+    // Function to randomly return either 'Rock', 'Paper', or 'Scissors'
+    function getComputerChoice() {
+        // Random number between 1 and 3 to map to the three choices, i.e. 1 = Rock, 2 = Paper, etc.
+        let computerChoice = Math.floor(Math.random() * 3) + 1
 
-    // Log the choice as the item, not the number
-    if (computerChoice === 1) {
-        console.log("Computer: Rock");
-    } else if (computerChoice === 2) {
-        console.log("Computer: Paper");
-    } else if (computerChoice === 3) {
-        console.log("Computer: Scissors")
+        // Log the choice as the item, not the number
+        if (computerChoice === 1) {
+            return "rock";
+        } else if (computerChoice === 2) {
+            return "paper";
+        } else if (computerChoice === 3) {
+            return "scissors";
+        }
+        }
+    // Function capitalize words
+    function toCapital(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     }
+
+    // Function to play a single round of Rock Paper Scissors
+    // Rock beats Scissors; Paper beats Rock; Scissors beat Paper.
+    function playRound(playerSelection, computerSelection) {
+
+        if (playerSelection === computerSelection) {
+            return `Whoa! It's a tie! You both chose ${playerSelection}.`;
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            return "You win! Paper beats Rock!";
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            return "You win! Rock beats Scissors!";
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            return "You win! Scissors beats Paper!";
+        } else {
+            return `You lose! ${toCapital(computerSelection)} beats ${toCapital(playerSelection)}.`;
+        }
+    }
+
+    // Prompt player for selection
+    let playerSelection = prompt("Rock, Paper, or Scissors?", '').toLowerCase();
+    // console.log(playerSelection);
+
+    // Get computer's selection from function call
+    let computerSelection = getComputerChoice();
+    // console.log(computerSelection);
+
+    // Log results to console
+    console.log(playRound(playerSelection, computerSelection));
 }
 
-getComputerChoice();
+// Play 5 rounds
+for (let round = 0; round < 5; round++) {
+    game()    
+}
