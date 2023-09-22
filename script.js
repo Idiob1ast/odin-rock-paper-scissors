8408040034be8f380d4b0d4ab6344cc5af3a5d8b
Idioblast <47544178+Idiob1ast@users.noreply.github.com>
2023-09-22 14:08:43 -0700

    Refactor code with extra functions and event listeners to produce GUI

--- script.js
+++ script.js
@@ -1,56 +1,70 @@
-function game() {
-  // Function to randomly return either 'Rock', 'Paper', or 'Scissors' for computer choice
-  function getComputerChoice() {
-    // Random number between 1 and 3 to map to the three choices, i.e. 1 = Rock, 2 = Paper, etc.
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("player-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_div = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
 
-    // Log the choice as the item, not the number
-    if (computerChoice === 1) {
-      return "rock";
-    } else if (computerChoice === 2) {
-      return "paper";
-    } else if (computerChoice === 3) {
-      return "scissors";
-    }
-  }
-
-  // Function to capitalize words
-  function toCapital(txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  }
-
-  // Function to play a single round of Rock Paper Scissors
-  // Rock beats Scissors; Paper beats Rock; Scissors beats Paper.
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      return `Whoa! It's a tie! You both chose ${playerSelection}.`;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      return "You win! Paper beats Rock!";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      return "You win! Rock beats Scissors!";
-    } else if (
-      playerSelection === "scissors" &&
-      computerSelection === "paper"
-    ) {
-      return "You win! Scissors beats Paper!";
-    } else {
-      return `You lose! ${toCapital(computerSelection)} beats ${toCapital(
-        playerSelection
-      )}.`;
-    }
-  }
-
-  // Prompt player for selection and make it lowercase for parsing
-  let playerSelection = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
-
-  // Get computer's selection from function call
-  let computerSelection = getComputerChoice();
-
-  // Log results to console
-  console.log(playRound(playerSelection, computerSelection));
+// Function to capitalize words
+function toCapital(txt) {
+  return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
 }
 
-// Play 5 rounds of the game
-for (let round = 0; round < 1; round++) {
-  game();
+// Function to randomly return either 'Rock', 'Paper', or 'Scissors' for computer choice
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  let randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
 }
+
+// Play a round of the game
+function playRound(playerChoice) {
+  let computerChoice = getComputerChoice();
+  if (playerChoice === computerChoice) {
+    result_div.innerHTML = `Whoa! It's a tie! You both chose ${toCapital(
+      playerChoice
+    )}.`;
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_div.innerHTML = "You win! Paper beats Rock!";
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_div.innerHTML = "You win! Rock beats Scissors!";
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_div.innerHTML = "You win! Scissors beats Paper!";
+  } else {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    result_div.innerHTML = `You lose! ${toCapital(
+      computerChoice
+    )} beats ${toCapital(playerChoice)}.`;
+  }
+  if (userScore === 5) {
+    alert("You won the game!");
+  } else if (computerScore === 5) {
+    alert("Computer wins!");
+  }
+}
+
+function main() {
+  rock_div.addEventListener("click", function () {
+    playRound("rock");
+  });
+
+  paper_div.addEventListener("click", function () {
+    playRound("paper");
+  });
+
+  scissors_div.addEventListener("click", function () {
+    playRound("scissors");
+  });
+}
+
+main();
